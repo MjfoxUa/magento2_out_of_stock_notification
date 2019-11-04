566c66171db5a7fa6b319727dc3115d3ccd53076
mjfoxua <mjworkingua@gmail.com>
2019-11-05 01:01:44 +0200

    Add Ajax Test files.

--- Block/ProductStockForm.php
+++ Block/ProductStockForm.php
@@ -6,23 +6,38 @@ namespace Plumrocket\OutOfStock\Block;
 
 class ProductStockForm extends \Magento\Framework\View\Element\Template
 {
+    /**
+     * @var \Magento\Framework\Registry
+     */
     protected $_registry;
+
+    /**
+     * @var \Magento\Store\Model\StoreManagerInterface
+     */
+    private $storeManager;
 
     public function __construct(
         \Magento\Backend\Block\Template\Context $context,
         \Magento\Framework\Registry $registry,
+        \Magento\Store\Model\StoreManagerInterface $storeManager,
         array $data = []
     )
     {
         $this->_registry = $registry;
+        $this->storeManager = $storeManager;
         parent::__construct($context, $data);
     }
 
-    public function _prepareLayout()
+    public function getBaseUrl()
     {
-        return parent::_prepareLayout();
+        return $this->storeManager->getStore()->getBaseUrl();
     }
 
+    public function getEmailData()
+    {
+        return $this->getEmail();
+    }
+    
     public function getCurrentCategory()
     {
         return $this->_registry->registry('current_category');
